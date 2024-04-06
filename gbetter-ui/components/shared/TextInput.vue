aeed07f922b6c0b5df2d893111303ba0e4ee735c
berkerkls <berkerkelesoglu@gmail.com>
2024-04-06 13:17:02 +0300

    landing page edit

--- gbetter-ui/components/shared/TextInput.vue
+++ gbetter-ui/components/shared/TextInput.vue
@@ -1,7 +1,40 @@
 <template>
-  <label class="input input-bordered flex items-center gap-2">
-    <input type="text" class="grow" placeholder="Username" />
-  </label>
+  <div class="flex flex-col gap-2">
+    <span v-if="props.labelName">{{ props.labelName }}</span>
+    <label
+      :class="`input input-bordered flex items-center gap-2 ${props.class}`"
+    >
+      <FontAwesome v-if="props.icon" :icon="props.icon" />
+      <input
+        v-model="modelValue"
+        :type="props.type === 'password' && passwordView ? 'text' : props.type"
+        :class="class"
+        :placeholder="props.placeholder"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <FontAwesome
+        class="cursor-pointer"
+        v-if="props.type === 'password'"
+        @click="passwordView = !passwordView"
+        :icon="!passwordView ? faEye : faEyeSlash"
+      />
+    </label>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+let props = defineProps([
+  "icon",
+  "modelValue",
+  "placeholder",
+  "labelName",
+  "type",
+  "isRequired",
+  "isValid",
+  "class",
+]);
+let emits = defineEmits(["update:modelValue"]);
+let modelValue = ref(props.modelValue);
+let passwordView = ref(false);
+</script>
--- gbetter-ui/pages/index.vue
+++ gbetter-ui/pages/index.vue
@@ -1,42 +1,34 @@
 <template>
   <div class="hero flex justify-center items-center h-full">
     <div class="hero-content flex-col-reverse lg:flex-row-reverse">
-      <div class="flex-1 card shrink-0 shadow-2xl bg-base-100">
-        <form class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              class="input input-bordered"
-              required
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              class="input input-bordered"
-              required
-            />
-            <label class="label">
-              <a href="#" class="label-text-alt link link-hover"
-                >Forgot password?</a
-              >
-            </label>
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary">Login</button>
-          </div>
-        </form>
+      <div class="card flex-1 shrink-0 shadow-2xl bg-base-100">
+        <div class="card shrink-0 shadow-1xl bg-base-100">
+          <form @submit.prevent="deneme" class="card-body">
+            <div class="form-control" v-for="(item, index) in loginFormInputs">
+              <SharedTextInput
+                :key="index"
+                v-model="item.value"
+                :icon="item.icon"
+                :class="item.class"
+                :is-required="item.isRequired"
+                :is-valid="item.isValid"
+                :label-name="item.labelName"
+                :placeholder="item.placeholder"
+                :type="item.type"
+              />
+            </div>
+            <div class="form-control mt-6">
+              <button type="submit" class="btn btn-primary text-white">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
       <div class="flex-1 text-center lg:text-left">
-        <h1 class="text-5xl font-bold">Login now!</h1>
+        <h1 class="text-5xl font-bold">
+          Start to achive your <span class="text-primary">goals</span> today!
+        </h1>
         <p class="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
@@ -48,7 +40,47 @@
 </template>
 
 <script setup>
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+
 definePageMeta({
   layout: "login",
 });
+
+const loginCommand = ref({ email: "", password: "" });
+
+const loginFormInputs = [
+  {
+    icon: faEnvelope,
+    key: "email",
+    value: "",
+    placeholder: "Email",
+    labelName: "Email",
+    type: "text",
+    isRequired: true,
+    isValid: false,
+    class: "grow",
+  },
+  {
+    icon: faKey,
+    key: "password",
+    value: "",
+    placeholder: "Password",
+    labelName: "Password",
+    type: "password",
+    isRequired: true,
+    isValid: false,
+    class: "grow",
+  },
+];
+
+const deneme = () => {
+  loginFormInputs.map((item) => {
+    if (Object.keys(loginCommand.value).includes(item.key)) {
+      console.log(item.value, "sss");
+      loginCommand.value[item.key] = item.value;
+    }
+  });
+  console.log(loginCommand.value, "command");
+};
 </script>
