14e87d3ffc8b988db2e845fbba12c09aca2f06fe
berkerkls <berkerkelesoglu@gmail.com>
2024-04-12 14:31:52 +0300

    editing dashboard table

--- gbetter-ui/components/TaskTable.vue
+++ gbetter-ui/components/TaskTable.vue
@@ -8,21 +8,23 @@
     <td>
       <div class="flex items-center gap-3">
         <div>
-          <div class="font-bold">Hart Hagerty</div>
-          <div class="text-sm opacity-50">United States</div>
+          <div class="font-bold">{{ props.title }}</div>
+          <div class="text-sm opacity-50">
+            {{ props.taskType === "0" ? "Task" : "Roadmap" }}
+          </div>
         </div>
       </div>
     </td>
     <td>
-      Zemlak, Daniel and Leannon
-      <br />
-      <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+      {{ props.startDate }}
     </td>
-    <td>Purple</td>
+    <td>{{ props.endDate }}</td>
     <th>
       <button class="btn btn-ghost btn-xs">details</button>
     </th>
   </tr>
 </template>
 
-<script setup></script>
+<script setup>
+let props = defineProps(["title", "taskType", "startDate", "endDate"]);
+</script>
--- gbetter-ui/pages/dashboard/index.vue
+++ gbetter-ui/pages/dashboard/index.vue
@@ -14,26 +14,14 @@
     <div class="flex flex-col lg:flex-row justify-center h-full">
       <StatsCard class="w-full lg:w-1/4 lg:h-3/4" />
       <div class="w-3/4 overflow-auto p-6">
-        <table v-if="allTasks.length > 0" class="table">
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" class="checkbox" />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <!-- rows -->
-            <TaskTable v-for="(item, index) in allTasks" />
-          </tbody>
-        </table>
-        <div role="alert" class="alert shadow-lg bg-accent">
+        <div v-if="loading" class="flex justify-center items-center h-full">
+          <span class="loading loading-spinner text-primary loading-lg"></span>
+        </div>
+        <div
+          v-if="!loading && !allTasks.length"
+          role="alert"
+          class="alert shadow-lg bg-accent"
+        >
           <FontAwesome :icon="faInfoCircle" clas="h-3 w-3" />
           <div>
             <h3 class="font-medium">You have not added a task yet.</h3>
@@ -44,8 +32,36 @@
           >
             Let's Add!
           </button>
-          <DialogsCreateTask />
+          <DialogsCreateTask @getTasks="getAllTasks()" />
         </div>
+        <table v-if="allTasks.length && !loading" class="table">
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" class="checkbox" />
+                </label>
+              </th>
+              <th>Title</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- rows -->
+            <TaskTable
+              v-for="(item, index) in allTasks"
+              :key="item.id"
+              :title="item.title"
+              :taskType="item.taskType"
+              :startDate="
+                item.startDate.substring(0, item.startDate.indexOf('T'))
+              "
+              :endDate="item.endDate.substring(0, item.endDate.indexOf('T'))"
+            />
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -60,11 +76,14 @@ definePageMeta({
 
 const allTasks = ref([]);
 const taskService = new TasksService();
+const loading = ref(true);
 
 const getAllTasks = () => {
+  loading.value = true;
   taskService.GetTasks().then((res) => {
     if (res.success) {
       allTasks.value = res.items;
+      loading.value = false;
     }
   });
 };
